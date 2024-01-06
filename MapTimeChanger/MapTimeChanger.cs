76f7448c7f43f75b20d364a6bf281302f80ece94
TICHOJEBEC <150498032+TICHOJEBEC-SK@users.noreply.github.com>
2024-01-06 11:12:20 +0100

    Add files via upload

---
+++ MapTimeChanger/MapTimeChanger.cs
@@ -0,0 +1,59 @@
+﻿namespace MapTimeChanger;
+
+using System;
+using System.IO;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Timers;
+
+public class PluginInfo : BasePlugin
+{
+    private Timer mapChangeTimer;
+
+    public override string ModuleName => "MapTimeChanger";
+    public override string ModuleVersion => "1.0";
+    public override string ModuleAuthor => "TICHOJEBEC";
+    public override string ModuleDescription => "Map changer, based on a timer.";
+
+    private string FilePath => Path.Join(ModuleDirectory, "maps.txt");
+
+    public override void Load(bool hotReload)
+    {
+        if (!File.Exists(FilePath))
+            File.Create(FilePath);
+
+        AddTimer(2700, () =>//2700 45 min
+        {
+            ChangeMap();
+        }, TimerFlags.REPEAT);
+
+        Console.WriteLine("MapTimeChanger is loaded");
+    }
+    private void ChangeMap()
+    {
+        Console.WriteLine("Changing map...");
+
+        try
+        {
+            string[] mapIdArray = File.ReadAllLines(FilePath);
+
+            if (mapIdArray.Length == 0)
+            {
+                Console.WriteLine("No map IDs found in file.");
+                return;
+            }
+
+            string randomMapId = mapIdArray[new Random().Next(mapIdArray.Length)];
+
+            Console.WriteLine($"Selected random map ID: {randomMapId}");
+            
+            Server.ExecuteCommand($"host_workshop_map {randomMapId}");
+
+            Console.WriteLine($"Executed command: host_workshop_map {randomMapId}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error changing map: {ex.Message}");
+        }
+    }
+}    
